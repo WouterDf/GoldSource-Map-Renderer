f6db5af46e7e018d00895b7f333360e8dc5e4d3d
Wouter Deferme <wouter.deferme@gmail.com>
2026-01-02 11:42:45 +0100

    Improve assetloader.

---
+++ src/assetloader.cpp
@@ -0,0 +1,62 @@
+#include <fstream>
+#include <ostream>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <filesystem>
+
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
+
+#include "assetloader.h"
+
+namespace AssetLoader {
+
+     std::filesystem::path getExecutablePath()
+     {
+        #ifdef _WIN32
+        #include <windows.h>
+            wchar_t buffer[MAX_PATH];
+            GetModuleFileNameW(nullptr, buffer, MAX_PATH);
+            return std::filesystem::path(buffer).parent_path();
+        #endif
+
+        #ifdef __linux__
+        #include <unistd.h>
+        #include <limits.h>
+            char buffer[PATH_MAX];
+            ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
+            buffer[len] = '\0';
+            return std::filesystem::path(buffer).parent_path();
+        #endif
+
+        #ifdef __APPLE__
+            uint32_t size = 0;
+            _NSGetExecutablePath(nullptr, &size); // get required size
+
+            std::string buffer(size, '\0');
+            _NSGetExecutablePath(buffer.data(), &size);
+
+            return std::filesystem::path(buffer).parent_path();
+        #endif
+     }
+
+     std::filesystem::path getAssetPath()
+     {
+          return getExecutablePath() / "assets";
+     }
+
+     std::string readAssetToString(std::filesystem::path relativePath)
+     {
+          std::ifstream file(getAssetPath() / relativePath, std::ios::in | std::ios::binary);
+          if( !file )
+          {
+               throw std::runtime_error("Failed to open file.");
+          }
+
+          std::ostringstream output;
+          output<< file.rdbuf();
+          return output.str();
+     }
+} // namespace AssetLoader
--- src/assetloader.h
+++ src/assetloader.h
@@ -1,62 +1,12 @@
 #pragma once
-
-#include <fstream>
-#include <ostream>
-#include <sstream>
-#include <stdexcept>
 #include <string>
 #include <filesystem>
-
-#ifdef __APPLE__
-#include <mach-o/dyld.h>
-#endif
-
 namespace AssetLoader {
 
-     std::filesystem::path getExecutablePath()
-     {
-        #ifdef _WIN32
-        #include <windows.h>
-            wchar_t buffer[MAX_PATH];
-            GetModuleFileNameW(nullptr, buffer, MAX_PATH);
-            return std::filesystem::path(buffer).parent_path();
-        #endif
+     std::filesystem::path getExecutablePath();
 
-        #ifdef __linux__
-        #include <unistd.h>
-        #include <limits.h>
-            char buffer[PATH_MAX];
-            ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer) - 1);
-            buffer[len] = '\0';
-            return std::filesystem::path(buffer).parent_path();
-        #endif
-
-        #ifdef __APPLE__
-            uint32_t size = 0;
-            _NSGetExecutablePath(nullptr, &size); // get required size
-
-            std::string buffer(size, '\0');
-            _NSGetExecutablePath(buffer.data(), &size);
-
-            return std::filesystem::path(buffer).parent_path();
-        #endif
-     }
-
-     std::filesystem::path getAssetPath()
-     {
-          return getExecutablePath() / "assets";
-     }
-
-     std::string readAssetToString(std::filesystem::path relativePath)
-     {
-          std::ifstream file(getAssetPath() / relativePath, std::ios::in | std::ios::binary);
-          if( !file )
-          {
-               throw std::runtime_error("Failed to open file.");
-          }
-
-          std::ostringstream output;
-          output<< file.rdbuf();
-          return output.str();
-     }
+     std::filesystem::path getAssetPath();
+     
+     std::string readAssetToString(std::filesystem::path relativePath);
+     
 } // namespace AssetLoader
